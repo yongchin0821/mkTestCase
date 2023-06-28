7bfa35643dc3d188797955e603ac664274f61034
Yongchin <yongchin39@qq.com>
2023-06-28 17:07:12 +0800

    chore: config update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="mkTestCase",
-    version="0.0.2",
+    version="0.0.3",
     author="Yongchin",
     author_email="yongchin39@qq.com",
     license="MIT",
